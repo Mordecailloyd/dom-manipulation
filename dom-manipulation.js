99a62b7fca5af60cd7b297532906f4d98e73ded1
Nick Bacon <mordecailloyd@gmail.com>
2018-04-16 14:06:41 -0700

    ttt playable no ui

--- dom-manipulation.js
+++ dom-manipulation.js
@@ -1,21 +1,23 @@
 
-	var N_SIZE = 3,
-		EMPTY = "&nbsp;",
-		boxes = [],
+	var
 		turn = "X",
 		score,
 		moves;
 
 
 	function set(e) {
-    debugger;
-		if (e.target.innerHTML !== EMPTY) {
-      e.target.innerHTML = turn
-      turn = turn === "X" ? "O" : "X";
-      moves += 1;
-			return;
+    // debugger;
+  		if (e.target.innerHTML == "") {
+        // debugger
+        e.target.innerHTML = turn
+        turn = turn === "X" ? "O" : "X";
+        e.target.className = "cell coloredcell";
+        moves += 1;
+  			return;
+		    }
 		}
 
+	// init();
   document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("grid").addEventListener("click", set, false);
   });
